59a0815d7b3d5ff7c3837fe85c2544a6f5e679c7
Zhiliang Gong <zhiliang.gong@gmail.com>
2017-09-19 09:32:40 -0500

    git ignore

--- softmax/main.py
+++ softmax/main.py
@@ -3,5 +3,4 @@ import math
 
 def softmax(L):
     scores = np.array(list(map(lambda x: math.exp(x), L)))
-    total_score = scores.sum()
-    return scores / total_score
+    return scores / scores.sum()
